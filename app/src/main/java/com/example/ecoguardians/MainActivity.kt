60e9c390fb82d23acf6fc8bcc65d7e5125822774
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-09-21 17:41:02 +0200

    add sign in/log in

--- app/src/main/java/com/example/ecoguardians/MainActivity.kt
+++ app/src/main/java/com/example/ecoguardians/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.ecoguardians
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.ecoguardians.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var signInButton: Button
+    private lateinit var logInButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
