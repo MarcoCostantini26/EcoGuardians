bd7b9804f04e20aa0d7f682cfc4ef661f79558c4
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-09-25 15:28:03 +0200

    adjust fragment

--- app/src/main/java/com/example/ecoguardians/MainActivity.kt
+++ app/src/main/java/com/example/ecoguardians/MainActivity.kt
@@ -3,54 +3,37 @@ package com.example.ecoguardians
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.ecoguardians.adapters.ViewPagerAdapter
 import com.example.ecoguardians.databinding.ActivityMainBinding
+import com.example.ecoguardians.ui.login.SigninFragment
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val fragmentLogIn = SigninFragment()
+        var transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_container, fragmentLogIn)
+        transaction.commit()
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener {
-            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener(){
+            val fragmentList = SecondFragment()
+            var transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_container, fragmentList)
+            transaction.commit()
         }
 
-        //findViewById<BottomAppBar>(R.id.bottomNavigation)[R.id.user].setOnClickListener{
-        //    navController.navigate(R.id.action_FirstFragment_to_userProfile)
-        //}
-        //setUpTabs()
 
     }
 
-    private fun setUpTabs() {
-        val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(UserProfile(), "Profilo")
-        binding.viewPager.adapter = adapter
-        binding.tabs.setupWithViewPager(binding.viewPager)
-
-        binding.tabs.getTabAt(3)!!.setIcon(R.drawable.user)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
