3bf50beaba9bd963a04572b7c0972c3cb8c5f1f1
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-09-25 16:33:57 +0200

    fix list overlapping

--- app/src/main/java/com/example/ecoguardians/MainActivity.kt
+++ app/src/main/java/com/example/ecoguardians/MainActivity.kt
@@ -2,38 +2,30 @@ package com.example.ecoguardians
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
 import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.ecoguardians.adapters.ViewPagerAdapter
-import com.example.ecoguardians.databinding.ActivityMainBinding
 import com.example.ecoguardians.ui.login.SigninFragment
-import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragmentLogIn = SigninFragment()
-        var transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_container, fragmentLogIn)
         transaction.commit()
 
+        // Transaction to the list of animals
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener(){
             val fragmentList = SecondFragment()
-            var transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_container, fragmentList)
-            transaction.commit()
+            val transaction2 : FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction2.replace(R.id.main_container, fragmentList)
+            transaction2.commit()
         }
 
-
     }
-
 }
